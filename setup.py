9d318398a35cbd255231d78f3f357b521daafc29
Iconic1009 <pk22613@gmail.com>
2025-06-02 20:30:05 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yt_thumbnail_downloader",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="Pritam Kumar",
+    author_email="pritamofficial7010@gmail.com",
+    description="A library to download YouTube thumbnails by scraping video pages",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/yt_thumbnail_downloader",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
